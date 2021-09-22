a055dd9880633a2f9f011339183b37d9b241d3d9
minh tram <minhtram0901@gmail.com>
2021-09-22 23:44:46 +0700

    clean code

--- src/bookingTicket/index.js
+++ src/bookingTicket/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import ReactDOM from "react-dom";
 import "../assets/css/index.css";
 import data from "../assets/data/danhSachGhe.json";
 import SeatRow from "./seatRow";
