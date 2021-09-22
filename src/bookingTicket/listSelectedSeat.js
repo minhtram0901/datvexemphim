7a0fa650357232a64ebaf8b8eecfaf7d4600eb64
minh tram <minhtram0901@gmail.com>
2021-09-22 23:21:12 +0700

    fix layout

--- src/bookingTicket/listSelectedSeat.js
+++ src/bookingTicket/listSelectedSeat.js
@@ -6,7 +6,6 @@ class ListSelectedSeat extends Component {
   render() {
     return (
       <React.Fragment>
-        {/* <!-- START --> */}
         <div className="d-flex flex-row align-items-center">
           <span className="gheDuocChon ml-3 mr-2 my-1"></span>
           <span style={{ fontSize: "20px" }}>Ghế đã đặt</span>
@@ -19,46 +18,51 @@ class ListSelectedSeat extends Component {
           <span className="ghe ml-3 mr-2 my-2"></span>
           <span style={{ fontSize: "20px" }}>Ghế chưa đặt</span>
         </div>
-
-        <table id="main-table" className="main-table table table-dark table-sm">
-          <thead>
-            <tr>
-              <th className="col-4">Số ghế</th>
-              <th className="col-4">Giá</th>
-              <th className="col-4">Hủy</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.dsGheDangDat.map((ghe) => {
-              return (
-                <tr>
-                  <td className="col-4">{ghe.soGhe}</td>
-                  <td className="col-4">{ghe.gia}</td>
-                  <td className="col-4">
-                    <button
-                      className="btnDelete"
-                      onClick={() => {
-                        this.props.deleteSeat(ghe.soGhe);
-                      }}
-                    >
-                      X
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-          <tfoot>
-            <tr className="text-warning">
-              <td>Tổng tiền</td>
-              <td></td>
-              <td>{this.props.dsGheDangDat.reduce((tongTien, ghe, index) => {
-                  return tongTien += ghe.gia;
-              }, 0)}</td>
-            </tr>
-          </tfoot>
-        </table>
-        {/* <!-- END --> */}
+        <div className="table-wrapper-scroll-y my-custom-scrollbar">
+          <table
+            id="main-table"
+            className="main-table table table-dark table-sm"
+          >
+            <thead>
+              <tr>
+                <th className="col-4">Số ghế</th>
+                <th className="col-4">Giá</th>
+                <th className="col-4">Hủy</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.dsGheDangDat.map((ghe) => {
+                return (
+                  <tr>
+                    <td className="col-4">{ghe.soGhe}</td>
+                    <td className="col-4">{ghe.gia}</td>
+                    <td className="col-4">
+                      <button
+                        className="btnDelete"
+                        onClick={() => {
+                          this.props.deleteSeat(ghe.soGhe);
+                        }}
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+            <tfoot>
+              <tr className="text-warning">
+                <td>Tổng tiền</td>
+                <td>
+                  {this.props.dsGheDangDat.reduce((tongTien, ghe, index) => {
+                    return (tongTien += ghe.gia);
+                  }, 0)}
+                </td>
+                <td></td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
       </React.Fragment>
     );
   }
